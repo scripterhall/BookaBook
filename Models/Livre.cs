4a7bd437708c66a1ceb922ad031d36cf36880231
Rabhi Mariem <rab.mariem@gmail.com>
2025-05-20 18:20:40 +0100

    apply changes

--- Models/Livre.cs
+++ Models/Livre.cs
@@ -9,32 +9,25 @@ namespace BookaBook.Models
         [Key]
         public Guid Id { get; set; } = Guid.NewGuid();
 
-        /*[Required]
-        [StringLength(100)]*/
         public string? Titre { get; set; }
 
-        /*[Required]*/
         public string? ISBN { get; set; } = String.Empty;
 
-        //[Required]
-        [StringLength(100)] 
         public string? Auteur { get; set; }
-        /*[Required]
-        [StringLength(100)]*/
-        public string? Description { get; set; } = String.Empty; 
+        public string? Description { get; set; } = String.Empty;
         public int? AnneePublication { get; set; }
         public string? ImageUrl { get; set; }
 
-        /*[ForeignKey("Category")]*/
+        [NotMapped]
+        public IFormFile? ImageFile { get; set; } // uploaded file
+
         public Guid? CategorieId { get; set; }
         public Category? Categorie { get; set; }
 
-        /*[Required]*/
         public int NombreExemplaires { get; set; }
 
-        /*[Required]*/
         public string? Langue { get; set; }
 
-        
+
     }
 }
